cde1544de06a9955e3264c4751a871f70e601bac
abdulrahmanbayo <ramhziworld@gmail.com>
2022-11-19 11:22:16 +0100

    Initial Commit

---
+++ shoppingApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  shoppingApp
+//
+//  Created by Kole-ibrahim Abdul rahman on 19/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ shoppingApp/shoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingAppApp.swift
+//  shoppingApp
+//
+//  Created by Kole-ibrahim Abdul rahman on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct shoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
